abd8e13ba25438edcc0835f381c17bf59fc62f70
Vũ Đức Tuyên <52770007+TuyenVukt@users.noreply.github.com>
2021-08-05 19:14:27 +0700

    Update test.java

--- src/main/java/com/projectmanager/controller/test.java
+++ src/main/java/com/projectmanager/controller/test.java
@@ -3,5 +3,6 @@ package com.projectmanager.controller;
 public class test {
     public static void main(String[] args) {
         System.out.println("pull request");
+        System.out.println("checked by tuyendz");
     }
 }
